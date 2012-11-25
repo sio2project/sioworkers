0b118b32ae618e4a786e61023ffa6809d26b3dbd
Maciej Matraszek <matraszek.maciej@gmail.com>
2012-11-25 15:30:16 +0100

    Bump version to 0.93
    Change-Id: If855662830bf7112146fa11e4b2f3a08680ac71e

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "sioworkers",
-    version = '0.92',
+    version = '0.93',
     author = "SIO2 Project Team",
     author_email = 'sio2@sio2project.mimuw.edu.pl',
     description = "Programming contest judging infrastructure",
