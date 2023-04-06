07d76e11557475b04ebb51497efda0448ec4023a
Jakub Kaszycki <jakub@kaszycki.net.pl>
2023-04-06 15:48:28 +0200

    Encoder-decoder task support

--- sio/executors/executor.py
+++ sio/executors/executor.py
@@ -1,7 +1,11 @@
 from __future__ import absolute_import
-from sio.executors import common
+from sio.executors import common, encdec_common
 from sio.workers.executors import SupervisedExecutor
 
 
 def run(environ):
     return common.run(environ, SupervisedExecutor())
+
+
+def encdec_run(environ):
+    return encdec_common.run(environ, SupervisedExecutor())
