16d5dc3c0a67e124f84bf0e4d935f151f6ad6fc9
Tomasz Waleń <walen@mimuw.edu.pl>
2023-06-05 10:31:23 +0200

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "sioworkers",
-    version = '1.4.1',
+    version = '1.4.2',
     author = "SIO2 Project Team",
     author_email = 'sio2@sio2project.mimuw.edu.pl',
     description = "Programming contest judging infrastructure",
